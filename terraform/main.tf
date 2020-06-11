c78db3674282451e1a188ad7d8ef7b7d0011645d
Jason Edwards <jtanium@gmail.com>
2020-06-11 15:51:22 -0600

    Initial terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+
+module "whalesay" {
+  source  = "my.scalr.com/env-stst3lhp1fhvffo/scalr_example/aws"
+  version = "0.1.0"
+
+  app_image = "jasonpayroc/whalesay"
+  dns_name = "${var.instance_name}.payroc.com"
+  instance_name = var.instance_name
+  instance_port = 8080
+}
